05b3cf665588105448c2f2bcc82e622ae3a94be9
pikesley <github@orgraphone.org>
2015-03-06 14:41:54 +0000

    An passing test

--- hello_kitty.rb
+++ hello_kitty.rb
@@ -1,8 +1,18 @@
+require 'json'
+require 'turbotlib'
+
 class HelloKitty
+
+  JESS = 'http://jess.openaddressesuk.org/infer'
+#  SOURCE = 'https://alpha.openaddressesuk.org/'
+  SOURCE = 'https://alpha.openaddressesuk.org/addresses.json?street=greenfinch+way&town=Horsham'
+
   def self.updates_since interval
-    [
-      '5eImTD',
-      '5GZNIf'
-    ]
+    response = HTTPClient.new.get SOURCE
+    j = JSON.parse(response.content)
+
+    tokens = j['addresses'].map { |p| p['url'].split('/').last }
+
+    tokens
   end
 end
--- spec/hello-kitty_spec.rb
+++ spec/hello-kitty_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe HelloKitty do
-  it 'gets IDs for addresses updated since the last run' do
+  it 'gets IDs for addresses updated since the last run', :vcr do
     expect(HelloKitty.updates_since Time.now - 3600).to eq [
       '5eImTD',
       '5GZNIf'
     ]
   end
+
+
 end

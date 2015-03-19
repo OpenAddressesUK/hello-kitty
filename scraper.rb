57e8df09d62564a55ed8884bf176e13a486ee5d8
James Smith <james@floppy.org.uk>
2015-03-19 15:31:09 +0000

    make scraper work

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,19 @@
 require 'json'
 require 'turbotlib'
+require './hello_kitty'
 
 Turbotlib.log("Starting run...")
 
+last_run = DateTime.parse(ENV['LAST_RUN_AT'])
+
 datas = []
-HelloKitty.uniques_since(nil).each do |token|
-  datas.push HelloKitty.infer(token)
+HelloKitty.updates_since(last_run).each do |token|
+  puts token
+  new_data = HelloKitty.infer(token)
+  puts new_data.length
+  datas.concat new_data
 end
 
-puts datas.inspect
-
-#puts tokens
-#puts JSON.dump(j)
+datas.each do |data|
+  puts JSON.dump(data)
+end

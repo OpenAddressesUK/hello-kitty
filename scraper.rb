3031a8f678a03cefe55a6e99487cd2ccd11a7ccd
pezholio <pezholio@gmail.com>
2015-04-23 08:55:19 +0100

    Request with retries to theodolite

--- scraper.rb
+++ scraper.rb
@@ -6,11 +6,16 @@ require 'hello_kitty'
 
 Turbotlib.log("Starting run...")
 
-last_run = DateTime.parse(ENV['LAST_RUN_AT']) rescue (Time.now - 86400)
+if ENV['FIRST_RUN']
+  last_run = (Time.now - 259200)
+else
+  last_run = DateTime.parse(ENV['LAST_RUN_AT'])
+end
+
+puts last_run
 
 HelloKitty.updates_since(last_run) do |token|
   HelloKitty.infer(token).each do |data|
     puts JSON.dump(data)
   end
 end
-
--- spec/hello-kitty_spec.rb
+++ spec/hello-kitty_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe HelloKitty do
-  
+
   it 'gets IDs for addresses updated since the last run', :vcr do
     Timecop.freeze(2015,3,19,11,24) do
       tokens = []
@@ -13,13 +13,24 @@ describe HelloKitty do
       expect(tokens.last).to eq "CvcUkx"
     end
   end
-  
+
+  it 'retries if there is a temporary issue with Theodolite' do
+    stub_request(:get, 'http://example.com').
+                to_return(status: 500).
+                times(5).then.
+                to_return(status: 200)
+
+    expect(HelloKitty).to receive(:sleep) { nil }.at_least(1).times
+
+    expect { HelloKitty.request_with_retries("http://example.com") }.to output("Hit error, trying again in 5 seconds\nHit error, trying again in 10 seconds\nHit error, trying again in 15 seconds\nHit error, trying again in 20 seconds\nHit error, trying again in 25 seconds\n").to_stderr
+  end
+
   it 'parses inference results for token', :vcr do
     results = HelloKitty.infer "rYwoGk"
     expect(results.count).to eq 34
     expect(results[0]['paon']).to eq 30
     expect(results[0]['provenance']['activity']["derived_from"][0]["type"]).to eq "inference"
   end
-  
+
 
 end
